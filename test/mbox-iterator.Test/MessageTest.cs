3ae2e3aa193111ddbd26bf757a8064de3c4dac01
Mikaël PLOUHINEC <Mikal PLOUHINEC>
2016-11-27 14:58:23 +0100

    Reorganize tests

---
+++ test/mbox-iterator.Test/MessageTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mbox_iterator.Data;
+using System.IO;
+
+namespace mbox_iterator.Test
+{
+    
+    [TestClass]
+    public class MessageTest
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGetMessagesFromEmptyFilePath()
+        {
+            string filePath = string.Empty;
+            var result = Message.GetMessages(filePath);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TestGetMessagesFromEmptyFileNotFound()
+        {
+            string filePath = @"Z:\blabla";
+            var result = Message.GetMessages(filePath);
+        }
+    }
+}
